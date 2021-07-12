9edfb1a384602e49a4b7d82fc1927c489486b4c5
Aman Dwivedi <19dwivediaman@gmail.com>
2021-07-12 22:51:10 +0530

    fb login

--- fastparcel/urls.py
+++ fastparcel/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('social_django.urls', namespace='social')),
     path('', views.home),
     path('sign-in/', auth_views.LoginView.as_view(template_name="sign_in.html")),
     path('sign-out/', auth_views.LogoutView.as_view(next_page='/')),
-    path('sign-up', views.sign_up),
+    path('sign-up/', views.sign_up),
     path('customer/', views.customer_page),
     path('courier/', views.courier_page)
 ]
