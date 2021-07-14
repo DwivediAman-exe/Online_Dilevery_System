990fe9d38646816dbb8f1cd3fa791dcefa2ed8c5
Aman Dwivedi <19dwivediaman@gmail.com>
2021-07-14 11:11:03 +0530

    password change

--- core/customer/views.py
+++ core/customer/views.py
@@ -4,9 +4,11 @@ from django.urls import reverse
 from core.customer import forms
 
 from django.contrib import messages
-
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
 
 # Create your views here.
+
 
 @login_required()
 def home(request):
@@ -17,18 +19,27 @@ def home(request):
 def profile_page(request):
     user_form = forms.BasicUserForm(instance=request.user)
     customer_form = forms.BasicCustomerForm(instance=request.user.customer)
+    password_from = PasswordChangeForm(request.user)
 
     if request.method == 'POST':
-        user_form = forms.BasicUserForm(
-            request.POST, instance=request.user)
-        customer_form = forms.BasicCustomerForm(
-            request.POST, request.FILES, instance=request.user.customer)
-        if user_form.is_valid() and customer_form.is_valid():
-            user_form.save()
-            customer_form.save()
+        if request.POST.get('action') == 'update_profile':
+            user_form = forms.BasicUserForm(
+                request.POST, instance=request.user)
+            customer_form = forms.BasicCustomerForm(
+                request.POST, request.FILES, instance=request.user.customer)
+            if user_form.is_valid() and customer_form.is_valid():
+                user_form.save()
+                customer_form.save()
+                messages.success(
+                    request, 'Profile has been updated successfully.')
+                return redirect(reverse('customer:profile'))
 
-            messages.success(
-                request, 'Profile has been updated successfully.')
-            return redirect(reverse('customer:profile'))
+        elif request.POST.get('action') == 'update_password':
+            password_from = PasswordChangeForm(request.user, request.POST)
+            if password_from.is_valid():
+                user = password_from.save()
+                update_session_auth_hash(request, user)
+                messages.success(request, 'Password changed successfully.')
+                return redirect(reverse('customer:profile'))
 
-    return render(request, 'customer/profile.html', {"user_form": user_form, "customer_form": customer_form})
+    return render(request, 'customer/profile.html', {"user_form": user_form, "customer_form": customer_form, "password_form": password_from})
